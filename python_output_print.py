f77e555f7d155346e5ec896bf5dc27f58c34fc31
sowndhar-m <sowndharrajm17@gmail.com>
2026-01-26 11:55:43 +0530

    https://github.com/sowndhar-m/New_learning_python.git

---
+++ python_output_print.py
@@ -0,0 +1,12 @@
+#<!-- You can use the print() function as many times as you want. Each call prints text on a new line by default -->
+
+#print("Hello World!")
+##print("I am learning Python.")
+#print("It is awesome!")
+
+#Double Quotes 
+#Text in Python must be inside quotes. You can use either " double quotes or ' single quotes:
+
+print("SowndharrajM")
+print("Hello World!", end=" ")
+print("I will print on the same line.")
