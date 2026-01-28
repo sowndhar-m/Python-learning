3e2d36d9804b330ccd0fc4c5e58b8ea814d74805
sowndhar-m <sowndharrajm17@gmail.com>
2026-01-28 13:44:29 +0530

    Array_information

---
+++ array.py
@@ -0,0 +1,17 @@
+#Array
+#arrays are used to store multiple values in one single variable. Its a special variable.Which can hold more than one value at a time.
+#We can access the values by refering to a indux number.
+#Examples 
+sports = ["cricket" , "football" , "tennis" , "kabaadi"]
+sports.append("Hockey") # a append mentode is useful for add the element to a array
+print(sports) #print the varial 
+sports.pop(2)
+print(sports)
+sports.remove("cricket")
+print(sports[0]) #access the element
+print(len(sports)) #Find the length of the array
+x = "football"
+#for x in (sports):
+# print(x)
+#Important : Python does not provide , separate build in array data type  like "C" or "Java".instead, python list can be used to store multiple datas in single variable and its work similary like array.
+
